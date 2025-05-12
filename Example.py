5a5d23f639367bd2265753255c162fd7da3ee762
Sina Parsa <Sina.P64@Outlook.com>
2025-05-12 23:11:27 +0330

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,12 @@
+##snake case
+name_course = "python"
+
+##camel case
+nameCourse = "java"
+
+##pascal case
+NameCourse = "php"
+
+print(name_course)
+print(nameCourse)
+print(NameCourse)
---
+++ Navngivning.py
@@ -0,0 +1,15 @@
+## For navngivning bruger man _ eksempelvis Lars Andersen skrives Lars_Andersen 
+##De forskellige navngivning måder::
+##snake_case 
+##camelCase
+##PascalCase
+
+##Example
+
+age = 19
+
+last_name = "Babak"
+
+print(age)
+
+print(last_name)
